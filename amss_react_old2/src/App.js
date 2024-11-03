8b23d93ea39095d7f24468ee7412ffd30f78fcde
Cosmina <cosminagheorghe47@gmail.com>
2024-11-03 15:50:56 +0200

    Add React folder

---
+++ amss_react_old2/src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import GroupForm from './components/GroupForm';
+
+function App() {
+  return (
+    <div>
+      <h1>Group Management</h1>
+      <GroupForm />
+    </div>
+  );
+}
+
+export default App;
